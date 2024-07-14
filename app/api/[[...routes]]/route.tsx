1f844251b701c4a22b318e8a917c111e3bcfebab
michaelchristwin <kelechukwuchristwin@gmail.com>
2024-07-14 10:39:55 +0100

    Moved endpoint

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -4,7 +4,6 @@ import { Button, Frog, TextInput } from "frog";
 import { devtools } from "frog/dev";
 import NFTABI from "@/ABI/Proxycontract.json";
 import { readContract } from "@wagmi/core";
-import cors from "cors";
 import { handle } from "frog/next";
 import { serveStatic } from "frog/serve-static";
 import { config } from "@/config/wagmiConfig";
@@ -14,8 +13,8 @@ import { Address } from "viem";
 const app = new Frog({
   title: "Impact Frames",
   assetsPath: "/",
-  basePath: "/api",
 
+  basePath: "/api",
   // Supply a Hub to enable frame verification.
   // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
 });
@@ -23,22 +22,13 @@ const app = new Frog({
 // Uncomment to use Edge Runtime
 // export const runtime = 'edge'
 let contractAdress: string;
-app.use(async (c, next) => {
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
-  });
-  console.log(`[${c.req.method}] ${c.req.url}`);
-  c.res.headers.set("Access-Control-Allow-Origin", "*");
-  await next();
-});
-app.frame("/", async (c) => {
+app.frame("/frame", async (c) => {
   const { buttonValue, inputText, status } = c;
   const fruit = inputText || buttonValue;
-  console.log(fruit);
+  //console.log(fruit);
   const query = c.req.query();
   contractAdress = query.id;
+  console.log(query.id);
   const result = await readContract(config, {
     abi: NFTABI,
     chainId: sepolia.id,
@@ -47,6 +37,7 @@ app.frame("/", async (c) => {
     functionName: "tokenURI",
   });
   const data = await (await fetch(result as string)).json();
+  console.log(data);
   return c.res({
     image: (
       <div
